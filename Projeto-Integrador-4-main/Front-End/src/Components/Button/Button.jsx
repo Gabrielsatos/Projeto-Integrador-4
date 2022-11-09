f8658c8f4b30bb70ce4af1fa1562f227c3510b64
Gabrielsatos <116702038+Gabrielsatos@users.noreply.github.com>
2022-11-09 00:30:24 -0300

    Delete Projeto-Integrador-4-main/Front-End directory

--- Projeto-Integrador-4-main/Front-End/src/Components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledButton } from "./Button.styled";
-
-const Button = ({ text }) => {
-    return <StyledButton>{text}</StyledButton>;
-  };
-
-export default Button;
--- Projeto-Integrador-4-main/Front-End/src/Components/Title/Title.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledTitle = styled.h3`
-  color: white;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-  font-weight: bold;
-`;
